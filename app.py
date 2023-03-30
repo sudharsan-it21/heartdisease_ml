03b0f4691e0d366c2dc6eac7c729b2a2b3bdc297
sudharsan-it21 <95199551+sudharsan-it21@users.noreply.github.com>
2023-03-30 18:17:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,45 +19,32 @@ if (selected == 'Heart Disease Prediction'):
     # page title
     st.title('Heart Disease Prediction ')
     
-    col1 = st.columns(1)
     
-    with col1:
+    
         age = st.text_input('Age')
-        
-    with col1:
+
         sex = st.text_input('Sex')
-        
-    with col1:
+
         cp = st.text_input('Chest Pain types')
-        
-    with col1:
+   
         trestbps = st.text_input('Resting Blood Pressure')
-        
-    with col1:
+  
         chol = st.text_input('Serum Cholestoral in mg/dl')
-        
-    with col1:
+   
         fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
-        
-    with col1:
+    
         restecg = st.text_input('Resting Electrocardiographic results')
-        
-    with col1:
+    
         thalach = st.text_input('Maximum Heart Rate achieved')
-        
-    with col1:
+    
         exang = st.text_input('Exercise Induced Angina')
-        
-    with col1:
+       
         oldpeak = st.text_input('ST depression induced by exercise')
-        
-    with col1:
+     
         slope = st.text_input('Slope of the peak exercise ST segment')
-        
-    with col1:
+      
         ca = st.text_input('Major vessels colored by flourosopy')
-        
-    with col1:
+     
         thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
         
         
