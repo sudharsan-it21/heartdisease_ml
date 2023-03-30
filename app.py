30e7c0cb47010b52d63c1ea99f0ade1568c964be
sudharsan-it21 <95199551+sudharsan-it21@users.noreply.github.com>
2023-03-30 18:40:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ with st.sidebar:
     
     selected = option_menu('Heart Disease Prediction System',
                           
-                          ['Heart Disease Prediction',
+                          ['',
                            ],
                           default_index=0)
     
